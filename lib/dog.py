b90d78580e97d22193a9ebde8faec619933f0217
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-05-12 10:39:34 +0300

    Add Dog and Person class implementations with methods for barking, sitting, talking, and walking

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,19 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
+
 
     #Instance method definition
-    pass
+pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,15 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+John = Person()
+John.talk()
+John.walk()
 
     #Instance method definition
-    pass
+pass
